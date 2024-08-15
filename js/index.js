e862f28bc409b3fc1b3b8b6ebd45c767a53a288e
pmaulidina <ninojuniansam@gmail.com>
2024-08-15 20:52:30 +0700

    commentar

--- js/index.js
+++ js/index.js
@@ -1,22 +1,27 @@
+//Menggunakan inputan jenis kelamin, berat, tinggi, dan umur
 function bmi() {
     const gender = document.querySelector('input[name="jenis-kelamin"]:checked');
     const berat = document.getElementById("berat").value;
     const tinggi = document.getElementById("tinggi").value;
     const umur = document.getElementById("umur").value;
 
+    // Memeriksa inputan, jika form tidak terisi code akan menampilkan peringatan dan hentikan fungsi
     if (!gender || !berat || !tinggi || !umur) {
         alert("Harap isi semua kolom!");
         return;
     }
 
+    // Mengonversi nilai berat dan tinggi menjadi float
     const beratFloat = parseFloat(berat);
     const tinggiFloat = parseFloat(tinggi);
 
+    // Memeriksa nilai berat & tinggi <= 0, jika ya, akan menampilkan peringatan dan menghentikan fungsi
     if (beratFloat <= 0 || tinggiFloat <= 0) {
         alert("Masukkan angka yang valid!");
         return;
     }
 
+    // Menghitung BMI
     const count = beratFloat / ((tinggiFloat / 100) * (tinggiFloat / 100));
     let BMIresult = '';
 
@@ -30,10 +35,12 @@ function bmi() {
         BMIresult = "Berat badan anda berada dalam kategori obesitas.";
     }
 
+    //Menampilkan hasil BMI dan deskripsi
     document.getElementById('BMIvalue').textContent = `${count.toFixed(2)}`;
     document.getElementById('BMIresult').textContent = BMIresult;
 }
 
+//Code untuk mereset tampilan
 function resetForm() {
     document.getElementById("bmiForm").reset();
     document.getElementById("BMIvalue").textContent = '';
